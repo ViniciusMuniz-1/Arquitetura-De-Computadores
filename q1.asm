124a57872140a4588a3a6dee698b04c151b714cb
Vinicius M <53084431+ViniciusMuniz-1@users.noreply.github.com>
2023-03-30 09:58:29 -0300

    Rename dobro.asm to q1.asm

--- q1.asm
+++ q1.asm
@@ -1,12 +1,12 @@
 .text
 
 main: addi $2, $0, 5
-      syscall		#le o número
+      syscall		#le o nÃºmero
       add $8, $0, $2	#passa o num para o reg $8
       
 dobro:add $4, $2, $8	#soma o num consigo mesmo
       addi $2, $0, 1
-      syscall		#imprime o dobro do número
+      syscall		#imprime o dobro do nÃºmero
       
 fim:  addi $2, $0, 10
       syscall
