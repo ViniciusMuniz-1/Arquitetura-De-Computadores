16f18f1d41f6b86b3ec098a9d7cebf4271ea4c40
Vinicius M <53084431+ViniciusMuniz-1@users.noreply.github.com>
2023-04-04 16:38:30 -0300

    Update q7.asm

--- q7.asm
+++ q7.asm
@@ -1,41 +1,44 @@
 .text
 
-main: addi $2, $0, 5
-      syscall
-      
-divpor1000: addi $8, $0, 1000 #adiciona 1000 ao reg 8
-	    div $2, $8        #divide o numero por 1000
-	    mflo $9	      #move a casa do milhar para o reg 9
-	    mfhi $10	      #move o resto do num para o reg 10
-	   
-divpor100:  sub $8, $0, 900  #deixa 100 no reg 8
-            div $10, $8      #divide o numero por 100
-            mflo $11         #move a centena para o reg 11
-            mfhi $12         #move o resto do num para o reg 12
-            
-divpor10:   sub $8, $0, 890  #deixa 10 no reg 8
-	    div $12, $8      #divide o num por 10
-	    mflo $13	     #move a dezena para o reg 13
-	    mfhi $14	     #move a unidade para o reg 14
-	    
-imprimir:   add $4, $0, $8
-	    addi $2, $0, 1
-	    syscall
-	    
-	    add $4, $0, $11
-	    syscall
-	    
-	    add $4, $0, $13
-	    syscall
-	    
-	    add $4, $0, $14
-	    syscall
-	    
-fim:        addi $2, $0 10
-	    syscall
-	    
-	    
+main:   addi $2, $0, 5 #le inteiro
+	syscall
+	addi $8, $0, 10 #guarda 10 no reg $8
 	
-	   
-	   
-	   
+	div $2, $8 #divide o valor de $2 pelo valor de $8
+	mfhi $4 #guarda o resto no reg $4
+	addi $2, $0, 1 #imprime o valor
+	syscall
+	add $4, $0, $8 #quebra linha
+	addi $2, $0, 11 #exibe caracter em $4
+	syscall
+	mflo $2 #armazena o valor de Lo em $2 (inteiro)
+	
+	div $2, $8 # divide o valor de $2 pelo valor de $8
+	mfhi $4 # armazena o valor de Hi em $4 (resto)
+	addi $2, $0, 1 # exibe o valor de $4
+	syscall
+	add $4, $0, $8 # quebra linha
+	addi $2, $0, 11 # exibe caracter em $4
+	syscall
+	mflo $2 # armazena o valor de Lo em $2 (inteiro)
+	
+	div $2, $8 # divide o valor de $2 pelo valor de $8
+	mfhi $4 # armazena o valor de Hi em $4 (resto)
+	addi $2, $0, 1 # exibe o valor de $4
+	syscall
+	add $4, $0, $8 # quebra linha
+	addi $2, $0, 11 # exibe caracter em $4
+	syscall
+	mflo $2 # armazena o valor de Lo em $2 (inteiro)
+	
+	div $2, $8 # divide o valor de $2 pelo valor de $8
+	mfhi $4 # armazena o valor de Hi em $4 (resto)
+	addi $2, $0, 1 # exibe o valor de $4
+	syscall
+	add $4, $0, $8 # quebra linha
+	addi $2, $0, 11 # exibe caracter em $4
+	syscall
+	mflo $2 # armazena o valor de Lo em $2 (inteiro)
+	
+	addi $2, $0, 10 # encerra programa
+	syscall
