fe1af398bf15901150164bd2ee5551d8077babb5
Vinicius M <53084431+ViniciusMuniz-1@users.noreply.github.com>
2023-04-05 10:21:05 -0300

    Add files via upload

--- q16.asm
+++ q16.asm
@@ -1,10 +1,25 @@
 .text
 
 main:  addi $2, $0, 5
-       syscall		#le
-       andi $8, $2, 1	#faz o and do numero com 1, se o resultado for 1 é impar, se for 0 é par
-       mul  $4, $8, -1  #multiplica por -1
+       syscall		#le o primeiro numero
+       add $8, $0, $2	#move o num1 para o reg 8
+       addi $2, $0, 5
+       syscall		#le o segundo numero
+       add $9, $0, $2	#move o num2 para o reg 9
+       
+       sub $10, $9, $8	
+       #subtração entre os 2 num, se for positivo num 2 é maior que num 1, caso não, num 1 é maior
+       add $11, $10, $0
+       not $11, $11
+       
+       srl $10, $10, 31
+       srl $11, $11, 31
+       
+       mul $12, $10, $9
+       mul $13, $11, $8
+       add $4, $12, $13
        addi $2, $0, 1
-       syscall		#imprime
+       syscall
+       
        addi $2, $0, 10
        syscall
