070db4715ed1d42e85c46f7a1fc53e0936076d74
Vinicius M <53084431+ViniciusMuniz-1@users.noreply.github.com>
2023-04-18 21:36:50 -0300

    Add files via upload

---
+++ lista2/q18l2.asm
@@ -0,0 +1,77 @@
+.text
+
+main:	addi $2, $0, 5
+	syscall
+	add $8, $2, $0
+	addi $25, $0, 2
+	
+	div $8, $25
+	mflo $8
+	mfhi $9		#bit menos sig no reg 9
+	
+	div $8, $25
+	mflo $8
+	mfhi $10	#bit meio reg 10
+	
+	div $8, $25
+	mflo $8
+	mfhi $11	#bit meio no reg 11
+	
+	div $8, $25
+	mflo $8
+	mfhi $12	#bit meio no reg 12
+	
+	div $8, $25
+	mflo $8
+	mfhi $13	#bit meio no reg 13
+	
+	div $8, $25
+	mflo $8
+	mfhi $14	#bit meio no reg 14
+	
+	div $8, $25
+	mflo $8
+	mfhi $15	#bit meio no reg 15
+	
+	div $8, $25
+	mflo $8		#bit mais sig no reg 8
+	mfhi $16	#bit menos sig no reg 16
+	
+impressao: add $4, $0, $8
+	   addi $2, $0, 1
+	   syscall
+	   
+	   add $4, $0, $16
+	   addi $2, $0, 1
+	   syscall
+	   
+	   add $4, $0, $15
+	   addi $2, $0, 1
+	   syscall
+	   
+	   add $4, $0, $14
+	   addi $2, $0, 1
+	   syscall
+	   
+	   add $4, $0, $13
+	   addi $2, $0, 1
+	   syscall
+	   
+	   add $4, $0, $12
+	   addi $2, $0, 1
+	   syscall
+	   
+	   add $4, $0, $11
+	   addi $2, $0, 1
+	   syscall
+	   
+	   add $4, $0, $10
+	   addi $2, $0, 1
+	   syscall
+	   
+	   add $4, $0, $9
+	   addi $2, $0, 1
+	   syscall
+	   
+	   addi $2, $0, 10
+	   syscall
